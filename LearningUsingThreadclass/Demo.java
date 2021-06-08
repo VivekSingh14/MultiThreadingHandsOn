22fc78cbaa2910330790caf940cdecde0dcbecce
vicky1411 <vivek.singh141@yahoo.com>
2021-06-08 10:36:16 +0530

    naming the thread~

--- LearningUsingThreadclass/Demo.java
+++ LearningUsingThreadclass/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
             System.out.println("We are now in thread "+Thread.currentThread().getName());
             }
         });
+    thread.setName("New worker thread");
     System.out.println("We are in thread: "+ Thread.currentThread().getName()+" before starting a new thread.");
     thread.start();
     System.out.println("We are in thread: "+ Thread.currentThread().getName()+" after starting a new thread.");

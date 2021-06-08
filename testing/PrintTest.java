bfad93ee761e5d4cccc3f88c486761e395bcc879
vicky1411 <vivek.singh141@yahoo.com>
2021-06-08 10:11:45 +0530

    second commit

--- testing/PrintTest.java
+++ testing/PrintTest.java
@@ -2,6 +2,7 @@ package testing;
 
 public class PrintTest {
     public static void main(String args[]){
+        //testing purposer only
         System.out.println("Testing multithreding repo");
     }
 }

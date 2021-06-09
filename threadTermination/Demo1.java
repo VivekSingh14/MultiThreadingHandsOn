802b913ab4361456ed8aa4a3f85376c94d3c7570
vicky1411 <vivek.singh141@yahoo.com>
2021-06-10 00:01:05 +0530

    added daemon thread

--- threadTermination/Demo1.java
+++ threadTermination/Demo1.java
@@ -5,6 +5,9 @@ import java.math.BigInteger;
 public class Demo1 {
     public static void main(String args[]){
         Thread thread = new Thread(new LongComputationTask(new BigInteger("10000"), new BigInteger("5000000")));
+        
+        //see below in pow method for explanation 
+        thread.setDaemon(true);
         thread.start();
         thread.interrupt();
     }
@@ -26,10 +29,11 @@ public class Demo1 {
             BigInteger result = BigInteger.ONE;
 
             for(BigInteger i=BigInteger.ZERO; i.compareTo(power) != 0; i= i.add(BigInteger.ONE)){
-                if(Thread.currentThread().isInterrupted()){
+                //we can set only simply set thread.setDaemon(true) instead of this if conditional.
+                /*if(Thread.currentThread().isInterrupted()){
                     System.out.println( "Prematurely interrupted computation.");
                     return BigInteger.ZERO;
-                }
+                }*/
                 result  = result.multiply(base);
             }
         return result;

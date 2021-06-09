a747d7e3dbdfaaea2519502ff14a0bcebb3e07b1
vicky1411 <vivek.singh141@yahoo.com>
2021-06-09 07:32:32 +0530

    exception handling in thread

---
+++ LearningUsingThreadclass/Demo2.java
@@ -0,0 +1,20 @@
+package LearningUsingThreadclass;
+
+public class Demo2 {
+    public static void main(String args[]){
+        Thread thread = new Thread(new Runnable(){
+            public void run(){
+                throw new RuntimeException("Intentional Exception .");
+            }
+        });
+        thread.setName("Misbehaving thread.");
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+            public void uncaughtException(Thread t, Throwable e){
+                System.out.println("A critical error happened in thread " + t.getName() 
+                        + " the error is " + e.getMessage());
+            }
+        });
+
+        thread.start();
+    }   
+}

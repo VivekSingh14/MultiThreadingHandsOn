d1bc4b4708a5d9440e8d6cc00a5255410b1def36
vicky1411 <vivek.singh141@yahoo.com>
2021-06-11 23:31:38 +0530

    successfully changed the color of flower.

--- PerformanceOptimization/src/ImageProcess.java
+++ PerformanceOptimization/src/ImageProcess.java
@@ -1,8 +1,66 @@
 package PerformanceOptimization.src;
 
-public class ImageProcess {
-    public static void main(String args[]){
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class ImageProcess {
+
+    public static final String SOURCE_FILE= "/home/vivek/Desktop/multiThreadingHandsOn/src/PerformanceOptimization/resources/many-flowers.jpg";
+    public static final String DESTINATION_FILE="/home/vivek/Desktop/multiThreadingHandsOn/src/PerformanceOptimization/out/many-flowers.jpg";
+    public static void main(String args[]) throws IOException{
+
+        BufferedImage originalImage = ImageIO.read(new File(SOURCE_FILE));
+        BufferedImage resultImage = new BufferedImage(originalImage.getWidth(), originalImage.getHeight(),BufferedImage.TYPE_INT_RGB);
+        
+        recolorSingleThreaded(originalImage, resultImage);
+
+        File outputFile = new File(DESTINATION_FILE);
+        ImageIO.write(resultImage, "jpg", outputFile);
+
+        //System.out.println(String.valueOf(duration));
+    }
+
+    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
+        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+    public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner, int width, int height){
+            for(int x = leftCorner; x < leftCorner+ width && x < originalImage.getWidth(); x++){
+                for(int y = topCorner; y < topCorner + height && y <originalImage.getHeight(); y++){
+                    recolorPixel(originalImage, resultImage, x, y);
+                }
+            }
+    }
+
+    public static void recolorPixel(BufferedImage originalImage, BufferedImage resultImage, int x, int y){
+        int rgb = originalImage.getRGB(x, y);
+
+        int red = getRed(rgb);
+        int green = getGreen(rgb);
+        int blue = getBlue(rgb);
+
+        int newRed;
+        int newGreen;
+        int newBlue;
+
+        if(isShadeOfGray(red, green, blue)){
+            newRed = Math.min(255, red + 10);
+            newGreen = Math.max(0, green-80);
+            newBlue = Math.max(0, blue-20);
+        }else{
+            newRed = red;
+            newGreen = green;
+            newBlue = blue;
+        }
+        int newRGB = createRGBFromColors(newRed, newGreen, newBlue);
+        setRGB(resultImage, x, y, newRGB);
+    }
+
+    public static void setRGB(BufferedImage image, int x, int y, int rgb){
+        image.getRaster().setDataElements(x, y, image.getColorModel().getDataElements(rgb, null));
     }
 
     public static boolean isShadeOfGray(int red, int green, int blue) {
